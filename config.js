8a183db3c5a18bc991938e437a0552c287e64f46
linhdungyyy <bacphiabac@gmail.com>
2025-01-12 21:52:47 +0700

    Update config.js...

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://m.me/dungdubai22' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
